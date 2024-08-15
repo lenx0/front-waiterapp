d9605dae69c923667b185bd9278230a236b88e4a
thiago <thiagofreitas1106@gmail.com>
2024-08-15 12:44:10 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export function App() {
-  return <h1>Hello World</h1>;
+import { Header } from './components/Header';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+export function App () {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+    </>
+  );
 }
