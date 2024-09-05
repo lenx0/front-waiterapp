d48c6b8bc19800815227ccbb98f7d03d80369e7e
thiago <thiagofreitas1106@gmail.com>
2024-09-05 14:17:05 -0300

    list users table

---
+++ src/types/User.ts
@@ -0,0 +1,25 @@
+// export interface Order {
+//   total: string
+//   _id: string
+//   table: string
+//   name: string
+//   createdAt: string;
+//   // status: 'WAITING' | 'IN_PRODUCTION' | 'DONE'
+//   status: string
+//   products: {
+//     _id: string
+//     quantity: number
+//     product: {
+//       name: string
+//       imagePath: string
+//       price: number
+//     }
+//   }[]
+
+// }
+
+export interface UserTypes {
+  _id: string
+  name: string
+  email: string
+}
